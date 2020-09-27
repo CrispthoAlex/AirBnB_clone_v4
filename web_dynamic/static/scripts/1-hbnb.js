afb9fee7eef9de2eaba4ba473a813695e305be2c
crispthoalex <alex877@gmail.com>
2020-09-27 02:00:49 +0000

    Add modified files * Fix input tag applying double quotes to values in html file * Fix code applyin each method to "$(this)" * Add attributes to .filter h4 to fix behavior of amenities list block

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -3,19 +3,22 @@ Select some Amenities to be comfortable
 */
 $(document).ready(function () {
   const amenityDict = {};
-  $('checkbox').on('change', function () {
-    const dataID = document.getElementById('data-id');
-    if ($(this).is(':checked')) {
-      // add Amenity ID to amenityDict
-      amenityDict[dataID] = document.getElementById('data-name');
-    } else {
-      // remove Amenity ID from amenityDict
-      delete amenityDict[dataID];
-    }
+  $('.amenities input:checkbox').on('change', function () {
+    $(this).each( function () {
+      const dataID = $(this).attr('data-id')
+      if (this.checked) {
+	// add Amenity ID to amenityDict
+	amenityDict[dataID] = $(this).attr('data-name')
+      } else {
+	// remove Amenity ID from amenityDict
+	delete amenityDict[dataID];
+      }
+    });
     if (Object.values(amenityDict).length > 0) {
       $('.amenities h4').text(Object.values(amenityDict).join(', '));
     } else {
-      $('.amenities h4').text('&nbsp;');
+      $('.amenities h4').html('&nbsp;');
     }
+
   });
 });
